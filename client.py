09df1c60b71d3d5cf92eed24bf746e6f0c580605
bariudin77 <bariudin77@gmail.com>
2019-12-04 22:46:37 +0200

    Need to add the file transport, and debug regex

--- client.py
+++ client.py
@@ -1,14 +1,42 @@
 import socket
+import sys
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest_ip = '192.168.43.43'
-dest_port = 8000
-s.connect((dest_ip, dest_port))
-msg = input()
-while not msg == 'quit':
+
+def sendtoserver(msg):
     s.send(msg.encode())
     data = ""
     data = s.recv(4096)
+    return data
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+dest_ip = '10.0.0.2'
+dest_port = 8000
+s.connect((dest_ip, dest_port))
+
+# if there is 4 args- go to the user mode '1'
+if len(sys.argv) is 4:
+    if sys.argv[1] is not '1':
+        print("Wrong argument- if you want to use, put '1' as the first arg")
+        exit(0)
+    msg = input("Search: ")
+    data = sendtoserver('$' + msg)
     print(data.decode())
+    msg = input("Choose: ")
+    data = sendtoserver('^' + msg)
+
+elif len(sys.argv) is 5:
+    if sys.argv[1] is not '0':
+        print("Wrong argument- if you want to listen, put '0' as the first arg")
+        exit(0)
     msg = input()
+    data = sendtoserver(msg)
+    print(data.decode())
+    listen_ip = sys.argv[2]
+    listen_port = sys.argv[4]
+    s.bind((listen_ip, listen_port))
+    s.listen(5)
+    client_socket, client_address = s.accept()
+    data = client_socket.recv(1024)
+
 s.close()
--- server.py
+++ server.py
@@ -6,7 +6,8 @@ import re
 
 INSERT_PATTERN = "^1\s[\s0-9\s]*\s[a-zA-Z_.,-;]+$"  # TODO fix the regex!!!
 SEARCH_PATTERN = "^2\s[\s0-9\sa-zA-Z_.,-=)(]+$"  # TODO fix the regex!!!
-
+USER_SEARCH = "^\$[\s0-9\sa-zA-Z_.,-=)(]+$"
+CHOOSE_PATTERN = "^\^[\s0-9\sa-zA-Z_.,-=)(]+$"
 '''
 Class Name: Handler
 Args type: data, socket, set_val
@@ -34,11 +35,17 @@ class Handler:
             self.set_val.process(self.data)
         # File search
         if re.match(SEARCH_PATTERN, self.data):
-            self.set_val.search(self.data, self.socket)
+            self.set_val.search(self.data[2:], self.socket)
+        # User search
+        if re.match(USER_SEARCH, self.data):
+            self.set_val.search(self.data[1:], self.socket)
+        # User choose
+        if re.match(CHOOSE_PATTERN, self.data):
+            self.set_val.choose(self.data[1:], self.socket)
 
 
 '''
-Class Name: Manager
+Class Name: Manager - ּStatic Class- Singleton
 Args type: Client address
 Return Val: None
 Info: Manage Clients Info- what files he posses
@@ -46,10 +53,21 @@ Info: Manage Clients Info- what files he posses
 
 
 class Manager:
+    __instance = None
+
+    @staticmethod
+    def getInstance(client_address):
+        if Manager.__instance is None:
+            Manager(client_address)
+        return Manager.__instance
 
     def __init__(self, client_addr):
-        self.dict = {}
-        self.addr = client_addr
+        if Manager.__instance is not None:
+            raise Exception("This class is singleton! ")
+        else:
+            Manager.__instance = self
+            self.dict = {}
+            self.addr = client_addr
 
     '''
     Name: set
@@ -87,11 +105,11 @@ class Manager:
         if self.dict is None:
             print("The word: " + str(data) + " don't exists - there is no such file name ")
         else:
-            to_search = str(data[2:])
+            # to_search = str(data[2:])
             msg = ""
             # sorts the massages to the client
             for k in sorted(self.dict.keys()):
-                if to_search in k:
+                if data in k:
                     msg = msg + str(k) + " " + str(self.dict[k]) + " , "
 
             socket.send(msg[:len(msg) - 2].encode())
@@ -104,35 +122,34 @@ Return Val:
 Info:
 '''
 if __name__ == "__main__":
+    '''
     # if there is 4 args- go to the user mode '1'
     if len(sys.argv) is 4:
-        if sys.argv[1] is not 0:
+        if sys.argv[1] is not '1':
             print("Wrong argument- if you want to use, put '1' as the first arg")
             exit(0)
+'''
+    server = socket(AF_INET, SOCK_STREAM)
+    server_ip = "10.0.0.2"
+    server_port = 8000
+    server.bind((server_ip, server_port))
+    server.listen(5)
 
-        server = socket(AF_INET, SOCK_STREAM)
-        server_ip = "192.168.43.43"
-        server_port = 8000
-        server.bind((server_ip, server_port))
-        server.listen(5)
+    while True:
+        client_socket, client_address = server.accept()
+        data = client_socket.recv(1024)
+        set_val = Manager.getInstance(client_address)
+        handler = Handler(data.decode(), client_socket, set_val)
+        handler.manage()
+        client_socket.close()
 
-        while True:
-            client_socket, client_address = server.accept()
-            data = client_socket.recv(1024)
-            set_val = Manager(client_address)
-            while not data == "":
-                handler = Handler(data.decode(), client_socket, set_val)
-                handler.manage()
-                client_socket.send(data.upper())  # to fix
-                data = client_socket.recv(1024)
-
-            print("Client Disconnected")
-            client_socket.close()
+'''
     # if there is 5 args- go to the listening mode '0'
     if len(sys.argv) is 5:
-        if sys.argv[1] is not 0:
+        if sys.argv[1] is not '0':
             print("Wrong argument- if you want to listen, put '0' as the first arg")
             exit(0)
-    
+
     else:
         print("Problem with args - try again")
+'''
